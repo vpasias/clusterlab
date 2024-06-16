5726a13fe91397ef62e351b97784842fa280a9d8
vpasias <52623837+vpasias@users.noreply.github.com>
2024-06-16 15:43:11 +0300

    Update vmhost_deployment.sh

--- vmhost_deployment.sh
+++ vmhost_deployment.sh
@@ -58,7 +58,7 @@ for i in {1..1}; do
     ssh_to "${i}" -t -- sudo apt update -y
     ssh_to "${i}" -t -- sudo apt upgrade -y
     ssh_to "${i}" -t -- sudo timedatectl set-timezone America/New_York
-    ssh_to "${i}" -t -- sudo apt-get install -y git vim net-tools wget curl bash-completion apt-utils iperf iperf3 mtr traceroute netcat sshpass socat python3 python2 python3-dev python2-dev
+    ssh_to "${i}" -t -- sudo apt-get install -y git vim net-tools wget curl bash-completion apt-utils iperf iperf3 mtr traceroute netcat sshpass socat python3-pip python3 python2 python3-dev python2-dev
 
     ssh_to "${i}" -t -- 'echo "root:gprm8350" | sudo chpasswd'
     ssh_to "${i}" -t -- 'echo "ubuntu:kyax7344" | sudo chpasswd'
