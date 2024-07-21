80abbc715f7137e2e4a6a2db643cfc2aaf11d7f0
vpasias <52623837+vpasias@users.noreply.github.com>
2024-07-21 16:42:01 +0300

    Update vm_deployment.sh

--- pros/vm_deployment.sh
+++ pros/vm_deployment.sh
@@ -6,11 +6,11 @@ kcli create network -c 172.90.0.0/24 service
 
 kcli create network -c 172.100.0.0/24 external
 
-kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,'{"size": 100, "interface": "sata"} ','{"size": 100, "interface": "sata"} ','{"size": 100, "interface": "sata"}'] -P nets=['{"name":"service","ip":"172.90.0.30","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] ceph0
+kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,'{"size": 100, "interface": "sata"}','{"size": 100, "interface": "sata"}','{"size": 100, "interface": "sata"}'] -P nets=['{"name":"service","ip":"172.90.0.30","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] ceph0
 
-kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,'{"size": 100, "interface": "sata"} ','{"size": 100, "interface": "sata"} ','{"size": 100, "interface": "sata"}'] -P nets=['{"name":"service","ip":"172.90.0.31","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] ceph1
+kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,'{"size": 100, "interface": "sata"}','{"size": 100, "interface": "sata"}','{"size": 100, "interface": "sata"}'] -P nets=['{"name":"service","ip":"172.90.0.31","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] ceph1
 
-kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,'{"size": 100, "interface": "sata"} ','{"size": 100, "interface": "sata"} ','{"size": 100, "interface": "sata"}'] -P nets=['{"name":"service","ip":"172.90.0.32","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] ceph2
+kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,'{"size": 100, "interface": "sata"}','{"size": 100, "interface": "sata"}','{"size": 100, "interface": "sata"}'] -P nets=['{"name":"service","ip":"172.90.0.32","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] ceph2
 
 kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.33","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] infra
 
