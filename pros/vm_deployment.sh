b3e577ce58eee6bf63d986ab24ea2aa2e5e3d73a
vpasias <52623837+vpasias@users.noreply.github.com>
2024-07-21 16:38:59 +0300

    Update vm_deployment.sh

--- pros/vm_deployment.sh
+++ pros/vm_deployment.sh
@@ -4,7 +4,7 @@ set -eux
 
 kcli create network -c 172.90.0.0/24 service
 
-kcli create network -c 172.90.1.0/24 external
+kcli create network -c 172.100.0.0/24 external
 
 kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,'{"size": 100, "interface": "sata"} ','{"size": 100, "interface": "sata"} ','{"size": 100, "interface": "sata"}'] -P nets=['{"name":"service","ip":"172.90.0.30","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony '] ceph0
 
