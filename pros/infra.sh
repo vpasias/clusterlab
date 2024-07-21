e9639d37f725ea51fbb3845748e0f065928a3d57
vpasias <52623837+vpasias@users.noreply.github.com>
2024-07-21 16:21:49 +0300

    Update infra.sh

--- pros/infra.sh
+++ pros/infra.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -eux
+
 kcli create network -c 172.90.0.0/24 service
 
 kcli create network -c 172.90.1.0/24 external
@@ -8,7 +12,7 @@ kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,'{"si
 
 kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,'{"size": 100, "interface": "sata"} ','{"size": 100, "interface": "sata"} ','{"size": 100, "interface": "sata"}'] -P nets=['{"name":"service","ip":"172.90.0.32","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony '] ceph2
 
-kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.33","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] deploy
+kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.33","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] infra
 
 kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.34","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] controller0
 
@@ -21,3 +25,16 @@ kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P n
 kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.38","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] compute1
 
 kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.39","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] compute2
+
+kcli ssh ceph0 'sudo tee -a /etc/hosts <<EOF
+172.90.0.30 ceph0.vipnet.vip ceph0
+172.90.0.31 ceph1.vipnet.vip ceph1
+172.90.0.32 ceph2.vipnet.vip ceph2
+172.90.0.33 infra.vipnet.vip infra
+172.90.0.34 controller0.vipnet.vip controller0
+172.90.0.35 controller1.vipnet.vip controller1
+172.90.0.36 controller2.vipnet.vip controller2
+172.90.0.37 compute0.vipnet.vip compute0
+172.90.0.38 compute1.vipnet.vip compute1
+172.90.0.39 compute2.vipnet.vip compute2
+EOF'
