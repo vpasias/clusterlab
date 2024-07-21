adf5ce5714410de77daa9c36de69079e209aee51
vpasias <52623837+vpasias@users.noreply.github.com>
2024-07-21 17:25:36 +0300

    Update vm_deployment.sh

--- pros/vm_deployment.sh
+++ pros/vm_deployment.sh
@@ -4,27 +4,27 @@ set -eux
 
 kcli create network -c 172.90.0.0/24 service
 
-kcli create network -c 172.100.0.0/24 -P dhcp=false -P dns=false external
+kcli create network -c 192.168.0.0/24 -P dhcp=false -P dns=false external
 
-kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,'{"size": 100, "interface": "sata"}','{"size": 100, "interface": "sata"}','{"size": 100, "interface": "sata"}'] -P nets=['{"name":"service","ip":"172.90.0.30","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] ceph0
+kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,100,100,100] -P nets=['{"name":"service","ip":"172.90.0.30","netmask":"24","gateway":"172.90.0.1"}','{"name":"external"}'] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony sshpass'] ceph0
 
-kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,'{"size": 100, "interface": "sata"}','{"size": 100, "interface": "sata"}','{"size": 100, "interface": "sata"}'] -P nets=['{"name":"service","ip":"172.90.0.31","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] ceph1
+kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,100,100,100] -P nets=['{"name":"service","ip":"172.90.0.31","netmask":"24","gateway":"172.90.0.1"}','{"name":"external"}'] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony sshpass'] ceph1
 
-kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,'{"size": 100, "interface": "sata"}','{"size": 100, "interface": "sata"}','{"size": 100, "interface": "sata"}'] -P nets=['{"name":"service","ip":"172.90.0.32","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] ceph2
+kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100,100,100,100] -P nets=['{"name":"service","ip":"172.90.0.32","netmask":"24","gateway":"172.90.0.1"}','{"name":"external"}'] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony sshpass'] ceph1
 
-kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.33","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] infra
+kcli create vm -i centos9stream -P memory=16384 -P numcpus=4 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.33","netmask":"24","gateway":"172.90.0.1"}','{"name":"external"}'] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony sshpass'] infra
 
-kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.34","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] controller0
+kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.34","netmask":"24","gateway":"172.90.0.1"}','{"name":"external"}'] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony sshpass'] controller0
 
-kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.35","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] controller1
+kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.35","netmask":"24","gateway":"172.90.0.1"}','{"name":"external"}'] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony sshpass'] controller1
 
-kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.36","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] controller2
+kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.36","netmask":"24","gateway":"172.90.0.1"}','{"name":"external"}'] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony sshpass'] controller2
 
-kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.37","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] compute0
+kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.37","netmask":"24","gateway":"172.90.0.1"}','{"name":"external"}'] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony sshpass'] compute0
 
-kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.38","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] compute1
+kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.38","netmask":"24","gateway":"172.90.0.1"}','{"name":"external"}'] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony sshpass'] compute1
 
-kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.39","netmask":"24","gateway":"172.90.0.1"}',external] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony'] compute2
+kcli create vm -i centos9stream -P memory=24576 -P numcpus=6 -P disks=[100] -P nets=['{"name":"service","ip":"172.90.0.39","netmask":"24","gateway":"172.90.0.1"}','{"name":"external"}'] -P cmds=['sudo yum -y update && sudo yum -y install vim chrony sshpass'] compute2
 
 kcli ssh ceph0 'sudo tee -a /etc/hosts <<EOF
 172.90.0.30 ceph0.vipnet.vip ceph0
@@ -161,3 +161,62 @@ kcli ssh ceph0 "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication 
 kcli ssh ceph0 "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
 kcli ssh ceph0 "sudo systemctl restart sshd"
 kcli ssh ceph0 "sudo rm -rf /root/.ssh/authorized_keys"
+
+kcli ssh ceph1 'echo "root:gprm8350" | sudo chpasswd'
+kcli ssh ceph1 "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+kcli ssh ceph1 "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+kcli ssh ceph1 "sudo systemctl restart sshd"
+kcli ssh ceph1 "sudo rm -rf /root/.ssh/authorized_keys"
+
+kcli ssh ceph2 'echo "root:gprm8350" | sudo chpasswd'
+kcli ssh ceph2 "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+kcli ssh ceph2 "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+kcli ssh ceph2 "sudo systemctl restart sshd"
+kcli ssh ceph2 "sudo rm -rf /root/.ssh/authorized_keys"
+
+kcli ssh infra 'echo "root:gprm8350" | sudo chpasswd'
+kcli ssh infra "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+kcli ssh infra "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+kcli ssh infra "sudo systemctl restart sshd"
+kcli ssh infra "sudo rm -rf /root/.ssh/authorized_keys"
+
+kcli ssh controller0 'echo "root:gprm8350" | sudo chpasswd'
+kcli ssh controller0 "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+kcli ssh controller0 "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+kcli ssh controller0 "sudo systemctl restart sshd"
+kcli ssh controller0 "sudo rm -rf /root/.ssh/authorized_keys"
+
+kcli ssh controller1 'echo "root:gprm8350" | sudo chpasswd'
+kcli ssh controller1 "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+kcli ssh controller1 "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+kcli ssh controller1 "sudo systemctl restart sshd"
+kcli ssh controller1 "sudo rm -rf /root/.ssh/authorized_keys"
+
+kcli ssh controller2 'echo "root:gprm8350" | sudo chpasswd'
+kcli ssh controller2 "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+kcli ssh controller2 "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+kcli ssh controller2 "sudo systemctl restart sshd"
+kcli ssh controller2 "sudo rm -rf /root/.ssh/authorized_keys"
+
+kcli ssh compute0 'echo "root:gprm8350" | sudo chpasswd'
+kcli ssh compute0 "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+kcli ssh compute0 "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+kcli ssh compute0 "sudo systemctl restart sshd"
+kcli ssh compute0 "sudo rm -rf /root/.ssh/authorized_keys"
+
+kcli ssh compute1 'echo "root:gprm8350" | sudo chpasswd'
+kcli ssh compute1 "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+kcli ssh compute1 "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+kcli ssh compute1 "sudo systemctl restart sshd"
+kcli ssh compute1 "sudo rm -rf /root/.ssh/authorized_keys"
+
+kcli ssh compute2 'echo "root:gprm8350" | sudo chpasswd'
+kcli ssh compute2 "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+kcli ssh compute2 "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+kcli ssh compute2 "sudo systemctl restart sshd"
+kcli ssh compute2 "sudo rm -rf /root/.ssh/authorized_keys"
+
+kcli list images
+kcli list pools
+kcli list networks
+kcli list vms
