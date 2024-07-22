4d286bfc8cd930825660524eb2b1b6b79cda0c3c
vpasias <52623837+vpasias@users.noreply.github.com>
2024-07-22 14:10:32 +0300

    Update cephpreq.sh

--- pros/cephpreq.sh
+++ pros/cephpreq.sh
@@ -11,8 +11,8 @@ systemctl restart chronyd
 #Ceph installation
 
 curl --silent --remote-name --location https://github.com/ceph/ceph/raw/quincy/src/cephadm/cephadm
- chmod +x cephadm
- #./cephadm add-repo --release reef
- ./cephadm add-repo --release quincy
- ./cephadm install
- ./cephadm install ceph-common
+chmod +x cephadm
+#./cephadm add-repo --release reef
+./cephadm add-repo --release quincy
+./cephadm install
+./cephadm install ceph-common
