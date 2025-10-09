78d11908a96779d113da7b4c48f68e7362b5f644
vpasias <52623837+vpasias@users.noreply.github.com>
2025-10-09 21:45:23 +0300

    Update deploy-microstack.sh

--- deploy-microstack.sh
+++ deploy-microstack.sh
@@ -107,10 +107,10 @@ ssh_to 1 -- sunbeam cluster add --name node-3.localdomain --output node-3.asc
 ssh_to 1 -- sunbeam cluster add --name node-4.localdomain --output node-4.asc
 ssh_to 1 -- sunbeam cluster add --name node-4.localdomain --output node-5.asc
 
-scp node-1.localdomain:"node2.asc" "node-2.localdomain:"
-scp node-1.localdomain:"node3.asc" "node-3.localdomain:"
-scp node-1.localdomain:"node4.asc" "node-4.localdomain:"
-scp node-1.localdomain:"node5.asc" "node-5.localdomain:"
+ssh_to 1 -- scp "node2.asc" "node-2.localdomain:"
+ssh_to 1 -- scp "node3.asc" "node-3.localdomain:"
+ssh_to 1 -- scp "node4.asc" "node-4.localdomain:"
+ssh_to 1 -- scp "node5.asc" "node-5.localdomain:"
 
 ssh_to 2 -t -- \
     time "cat 'node-2.asc' | sunbeam cluster join --role control,compute,storage -"
