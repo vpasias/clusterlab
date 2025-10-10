8978e02fc4178fdfb236ca35dd4d03ec179ffaa7
vpasias <52623837+vpasias@users.noreply.github.com>
2025-10-10 18:16:16 +0300

    Update deploy-microstack-basic.sh

--- deploy-microstack-basic.sh
+++ deploy-microstack-basic.sh
@@ -165,3 +165,87 @@ done
 
 ssh_to 1 -t -- 'tee deployment_manifest.yaml' < umanifest.yaml
 #ssh_to 1 -t -- 'tail -n+2 /snap/openstack/current/etc/manifests/edge.yml >> deployment_manifest.yaml'
+
+ssh_to 1 -t -- \
+    time sunbeam cluster bootstrap --manifest deployment_manifest.yaml \
+        --role control --role compute --role storage
+
+#ssh_to 1 -t -- juju destroy-controller localhost-localhost --no-prompt
+#ssh_to 1 -t -- lxc profile device remove default eth0
+#ssh_to 1 -t -- lxc network delete sunbeambr0
+
+# LP: #2065490
+ssh_to 1 -t -- 'juju model-default --cloud sunbeam-microk8s logging-config="<root>=INFO;unit=DEBUG"'
+ssh_to 1 -t -- 'juju model-config -m openstack logging-config="<root>=INFO;unit=DEBUG"'
+ssh_to 1 -t -- sunbeam configure --openrc demo-openrc
+
+ssh_to 1 -t -- sunbeam cluster add --name node-2.localdomain --output node-2.asc
+ssh_to 1 -t -- sunbeam cluster add --name node-3.localdomain --output node-3.asc
+ssh_to 1 -t -- sunbeam cluster add --name node-4.localdomain --output node-4.asc
+ssh_to 1 -t -- sunbeam cluster add --name node-4.localdomain --output node-5.asc
+
+ssh_to 1 -t -- 'sshpass -p kyax7344 scp node2.asc ubuntu@node-2:/home/ubuntu'
+ssh_to 1 -t -- 'sshpass -p kyax7344 scp node3.asc ubuntu@node-2:/home/ubuntu'
+ssh_to 1 -t -- 'sshpass -p kyax7344 scp node4.asc ubuntu@node-2:/home/ubuntu'
+ssh_to 1 -t -- 'sshpass -p kyax7344 scp node5.asc ubuntu@node-2:/home/ubuntu'
+
+ssh_to 2 -t -- \
+    time "cat 'node-2.asc' | sunbeam cluster join --role control,compute,storage -"
+
+ssh_to 3 -t -- \
+    time "cat 'node-3.asc' | sunbeam cluster join --role control,compute,storage -"
+
+ssh_to 4 -t -- \
+    time "cat 'node-4.asc' | sunbeam cluster join --role compute,storage -"
+
+ssh_to 5 -t -- \
+    time "cat 'node-5.asc' | sunbeam cluster join --role compute,storage -"
+
+ssh_to 1 -t -- \
+    time sunbeam cluster resize
+
+ssh_to 1 -t -- sunbeam launch ubuntu --name test
+
+sleep 5m
+ssh_to 1 -t -- '
+    set -ex
+    source demo-openrc
+    demo_floating_ip="$(openstack floating ip list -c Floating\ IP\ Address -f value | head -n1)"
+    ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i ~/snap/openstack/current/sunbeam "ubuntu@${demo_floating_ip}" -- systemctl is-system-running --wait
+'
+
+for i in {1..5}; do
+    ssh_to "${i}" -t -- \
+        'time sunbeam openrc > admin-openrc'
+done
+
+for i in {1..5}; do
+    ssh_to "${i}" -t -- '
+        set -x 
+        mkdir logs
+        cd logs/
+        rsync -arv ~/snap/openstack/common/logs/ snap_openstack_common_logs/
+        cp -v ../tempest-validation*.log .
+        snap list | tee snap_list.txt
+        sunbeam cluster list | tee sunbeam_cluster_list.txt
+        sudo microceph status | tee microceph_status.txt
+        sudo microceph disk list | tee microceph_disk_list.txt
+        sudo ceph status | tee ceph_status.txt
+        sudo ceph health detail | tee ceph_health_detail.txt
+        sudo ceph osd pool autoscale-status | tee ceph_autoscale_status.txt
+        sudo k8s status | tee k8s_status.txt
+        sudo k8s kubectl get pod -A -o custom-columns=PodName:.metadata.name,PodUID:.metadata.uid > k8s_kubectl_get_pod_-A_custom.txt
+        sudo k8s kubectl get all -A > k8s_kubectl_get_all_-A.txt
+        sudo k8s kubectl describe all -A > k8s_kubectl_describe_all_-A.txt
+        systemd-cgtop -c --cpu=time -1 > systemd-cgtop_-c_--cpu_time_-1.txt
+        systemd-cgtop -m -1 > systemd-cgtop_-m_-1.txt
+        juju controllers --refresh | tee juju_controllers.txt
+        mkdir juju/
+        for model in $(juju models --format json | jq -r .models[].name); do
+            juju status -m "$model" --relations > "juju/status_${model##*/}.txt"
+            juju status -m "$model" --color | tee "juju/status_${model##*/}_color.txt"
+            juju debug-log -m "$model" --date --replay > "juju/debug_${model##*/}.log"
+        done
+        '
+         rsync -ar --mkpath "node-${i}.localdomain:logs/" "logs/node-${i}.localdomain"
+done
