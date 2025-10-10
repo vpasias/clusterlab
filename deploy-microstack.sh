83a1f65fcd4efd63df509403b58588bf9bfc843b
vpasias <52623837+vpasias@users.noreply.github.com>
2025-10-10 11:16:18 +0300

    Update deploy-microstack.sh

--- deploy-microstack.sh
+++ deploy-microstack.sh
@@ -77,18 +77,40 @@ for i in {1..5}; do
     virsh start "node-${i}.localdomain"
 done
 
+for i in {1..5}; do
+    sudo -g libvirt virsh -c qemu:///system start "node-${i}.localdomain"
+done
+
+for i in {1..5}; do
+     until ssh -oStrictHostKeyChecking=no "node-${i}.localdomain" -- 'systemctl is-system-running --wait; ip -br a; lsblk'; do
+     sleep 5
+     done
+done
 
 for i in {1..5}; do
     until ssh_to "${i}" -t -- cloud-init status --wait; do
         sleep 5
     done
 
+    ssh_to "${i}" -t -- 'sudo install -m 0600 /dev/stdin /etc/netplan/90-local-ovs-ext-port.yaml <<EOF
+          network:
+            version: 2
+            ethernets:
+              # LP: #2065911
+              enp9s0:
+                dhcp4: false
+                dhcp6: false
+                accept-ra: false
+          EOF
+          sudo netplan apply'
+    done
+
     ssh_to "${i}" -t -- sudo snap install openstack --channel 2024.1/edge
     ssh_to "${i}" -t -- 'sunbeam prepare-node-script | bash -x'
 
-    # LP: #2065911
-    # TODO: make it permanent across reboots
-    ssh_to "${i}" -- sudo ip link set enp9s0 up
+# LP: #2065911
+# TODO: make it permanent across reboots
+#    ssh_to "${i}" -- sudo ip link set enp9s0 up
 done
 
 ssh_to 1 -- 'tee deployment_manifest.yaml' < umanifest.yaml
@@ -98,19 +120,24 @@ ssh_to 1 -t -- \
     time sunbeam cluster bootstrap --manifest deployment_manifest.yaml \
         --role control --role compute --role storage
 
+ssh_to 1 -t -- juju destroy-controller localhost-localhost --no-prompt
+ssh_to 1 -t -- lxc profile device remove default eth0
+ssh_to 1 -t -- lxc network delete sunbeambr0
+
 # LP: #2065490
 ssh_to 1 -- 'juju model-default --cloud sunbeam-microk8s logging-config="<root>=INFO;unit=DEBUG"'
 ssh_to 1 -- 'juju model-config -m openstack logging-config="<root>=INFO;unit=DEBUG"'
+ssh_to 1 -- sunbeam configure --openrc demo-openrc
 
 ssh_to 1 -- sunbeam cluster add --name node-2.localdomain --output node-2.asc
 ssh_to 1 -- sunbeam cluster add --name node-3.localdomain --output node-3.asc
 ssh_to 1 -- sunbeam cluster add --name node-4.localdomain --output node-4.asc
 ssh_to 1 -- sunbeam cluster add --name node-4.localdomain --output node-5.asc
 
-ssh_to 1 -- scp "node2.asc" "node-2.localdomain:"
-ssh_to 1 -- scp "node3.asc" "node-3.localdomain:"
-ssh_to 1 -- scp "node4.asc" "node-4.localdomain:"
-ssh_to 1 -- scp "node5.asc" "node-5.localdomain:"
+scp node-1.localdomain:"node2.asc" "node-2.localdomain:"
+scp node-1.localdomain:"node3.asc" "node-3.localdomain:"
+scp node-1.localdomain:"node4.asc" "node-4.localdomain:"
+scp node-1.localdomain:"node5.asc" "node-5.localdomain:"
 
 ssh_to 2 -t -- \
     time "cat 'node-2.asc' | sunbeam cluster join --role control,compute,storage -"
@@ -127,27 +154,51 @@ ssh_to 5 -t -- \
 ssh_to 1 -t -- \
     time sunbeam cluster resize
 
-ssh_to 1 -t -- \
-    time sunbeam configure --openrc demo-openrc --manifest deployment_manifest.yaml
+ssh_to 1 -t -- sunbeam launch ubuntu --name test
+
+sleep 5m
+ssh_to 1 -t -- '
+    set -ex
+    source demo-openrc
+    demo_floating_ip="$(openstack floating ip list -c Floating\ IP\ Address -f value | head -n1)"
+    ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i ~/snap/openstack/current/sunbeam "ubuntu@${demo_floating_ip}" -- systemctl is-system-running --wait
+'
 
 for i in {1..5}; do
     ssh_to "${i}" -t -- \
         'time sunbeam openrc > admin-openrc'
 done
 
-ssh_to 1 -t -- \
-    time sunbeam launch ubuntu --name test
-
-# shellcheck disable=SC2016
-ssh_to 1 -t -- '
-    set -ex
-    # The cloud-init process inside the VM takes ~2 minutes to bring up the
-    # SSH service after the VM gets ACTIVE in OpenStack
-    sleep 300
-    source demo-openrc
-    demo_floating_ip="$(openstack floating ip list -c Floating\ IP\ Address -f value | head -n1)"
-    ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -i ~/snap/openstack/current/sunbeam "ubuntu@${demo_floating_ip}" true
-'
+for i in {1..5}; do
+    ssh_to "${i}" -t -- '
+        set -x 
+        mkdir logs
+        cd logs/
+        rsync -arv ~/snap/openstack/common/logs/ snap_openstack_common_logs/
+        cp -v ../tempest-validation*.log .
+        snap list | tee snap_list.txt
+        sunbeam cluster list | tee sunbeam_cluster_list.txt
+        sudo microceph status | tee microceph_status.txt
+        sudo microceph disk list | tee microceph_disk_list.txt
+        sudo ceph status | tee ceph_status.txt
+        sudo ceph health detail | tee ceph_health_detail.txt
+        sudo ceph osd pool autoscale-status | tee ceph_autoscale_status.txt
+        sudo k8s status | tee k8s_status.txt
+        sudo k8s kubectl get pod -A -o custom-columns=PodName:.metadata.name,PodUID:.metadata.uid > k8s_kubectl_get_pod_-A_custom.txt
+        sudo k8s kubectl get all -A > k8s_kubectl_get_all_-A.txt
+        sudo k8s kubectl describe all -A > k8s_kubectl_describe_all_-A.txt
+        systemd-cgtop -c --cpu=time -1 > systemd-cgtop_-c_--cpu_time_-1.txt
+        systemd-cgtop -m -1 > systemd-cgtop_-m_-1.txt
+        juju controllers --refresh | tee juju_controllers.txt
+        mkdir juju/
+        for model in $(juju models --format json | jq -r .models[].name); do
+            juju status -m "$model" --relations > "juju/status_${model##*/}.txt"
+            juju status -m "$model" --color | tee "juju/status_${model##*/}_color.txt"
+            juju debug-log -m "$model" --date --replay > "juju/debug_${model##*/}.log"
+        done
+        '
+         rsync -ar --mkpath "node-${i}.localdomain:logs/" "logs/node-${i}.localdomain"
+done
 
 # be nice to my SSD
-ssh_to 1 -t -- juju model-config -m openstack update-status-hook-interval=2h
+#ssh_to 1 -t -- juju model-config -m openstack update-status-hook-interval=2h
