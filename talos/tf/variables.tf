6eaa9e19ec9d54f2615e2d1d59ade24b71296bef
vpasias <52623837+vpasias@users.noreply.github.com>
2026-01-22 10:46:33 +0200

    Create variables.tf

---
+++ talos/tf/variables.tf
@@ -0,0 +1,115 @@
+# see https://github.com/siderolabs/talos/releases
+# see https://docs.siderolabs.com/talos/v1.12/getting-started/support-matrix
+variable "talos_version" {
+  type = string
+  # renovate: datasource=github-releases depName=siderolabs/talos
+  default = "1.12.1"
+  validation {
+    condition     = can(regex("^\\d+(\\.\\d+)+", var.talos_version))
+    error_message = "Must be a version number."
+  }
+}
+
+# see https://github.com/siderolabs/kubelet/pkgs/container/kubelet
+# see https://docs.siderolabs.com/talos/v1.12/getting-started/support-matrix
+variable "kubernetes_version" {
+  type = string
+  # renovate: datasource=github-releases depName=siderolabs/kubelet
+  default = "1.35.0"
+  validation {
+    condition     = can(regex("^\\d+(\\.\\d+)+", var.kubernetes_version))
+    error_message = "Must be a version number."
+  }
+}
+
+variable "cluster_name" {
+  description = "A name to provide for the Talos cluster"
+  type        = string
+  default     = "athena"
+}
+
+variable "cluster_vip" {
+  description = "The virtual IP (VIP) address of the Kubernetes API server. Ensure it is synchronized with the 'cluster_endpoint' variable."
+  type        = string
+  default     = "10.17.3.9"
+}
+
+variable "cluster_endpoint" {
+  description = "The virtual IP (VIP) endpoint of the Kubernetes API server. Ensure it is synchronized with the 'cluster_vip' variable."
+  type        = string
+  default     = "https://10.17.3.9:6443"
+}
+
+variable "cluster_node_network" {
+  description = "The IP network of the cluster nodes"
+  type        = string
+  default     = "10.17.3.0/24"
+}
+
+variable "cluster_node_network_first_controller_hostnum" {
+  description = "The hostnum of the first controller host"
+  type        = number
+  default     = 80
+}
+
+variable "cluster_node_network_first_worker_hostnum" {
+  description = "The hostnum of the first worker host"
+  type        = number
+  default     = 90
+}
+
+variable "cluster_node_network_load_balancer_first_hostnum" {
+  description = "The hostnum of the first load balancer host"
+  type        = number
+  default     = 130
+}
+
+variable "cluster_node_network_load_balancer_last_hostnum" {
+  description = "The hostnum of the last load balancer host"
+  type        = number
+  default     = 230
+}
+
+variable "cluster_node_domain" {
+  description = "the DNS domain of the cluster nodes"
+  type        = string
+  default     = "talos.test"
+}
+
+variable "ingress_domain" {
+  description = "the DNS domain of the ingress resources"
+  type        = string
+  default     = "example.test"
+}
+
+variable "controller_count" {
+  type    = number
+  default = 3
+  validation {
+    condition     = var.controller_count >= 1
+    error_message = "Must be 1 or more."
+  }
+}
+
+variable "worker_count" {
+  type    = number
+  default = 3
+  validation {
+    condition     = var.worker_count >= 1
+    error_message = "Must be 1 or more."
+  }
+}
+
+variable "talos_libvirt_base_volume_name" {
+  type    = string
+  default = "talos-1.12.1.qcow2"
+  validation {
+    condition     = can(regex(".+\\.qcow2+$", var.talos_libvirt_base_volume_name))
+    error_message = "Must be a name with a .qcow2 extension."
+  }
+}
+
+variable "prefix" {
+  type    = string
+  default = "athena_talos"
+}
