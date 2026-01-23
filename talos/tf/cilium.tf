3c73940a4cad7d03487ba323a136b0af83581f4e
vpasias <52623837+vpasias@users.noreply.github.com>
2026-01-23 18:41:07 +0200

    Create cilium.tf

---
+++ talos/tf/cilium.tf
@@ -0,0 +1,134 @@
+locals {
+  # see https://docs.cilium.io/en/stable/network/lb-ipam/
+  # see https://docs.cilium.io/en/stable/network/l2-announcements/
+  # see the CiliumL2AnnouncementPolicy type at https://github.com/cilium/cilium/blob/v1.18.6/pkg/k8s/apis/cilium.io/v2alpha1/l2announcement_types.go#L23-L42
+  # see the CiliumLoadBalancerIPPool type at https://github.com/cilium/cilium/blob/v1.18.6/pkg/k8s/apis/cilium.io/v2alpha1/lbipam_types.go#L22-L46
+  cilium_external_lb_manifests = [
+    {
+      apiVersion = "cilium.io/v2alpha1"
+      kind       = "CiliumL2AnnouncementPolicy"
+      metadata = {
+        name = "external"
+      }
+      spec = {
+        loadBalancerIPs = true
+        interfaces = [
+          "eth0",
+        ]
+        nodeSelector = {
+          matchExpressions = [
+            {
+              key      = "node-role.kubernetes.io/control-plane"
+              operator = "DoesNotExist"
+            },
+          ]
+        }
+      }
+    },
+    {
+      apiVersion = "cilium.io/v2alpha1"
+      kind       = "CiliumLoadBalancerIPPool"
+      metadata = {
+        name = "external"
+      }
+      spec = {
+        blocks = [
+          {
+            start = cidrhost(var.cluster_node_network, var.cluster_node_network_load_balancer_first_hostnum)
+            stop  = cidrhost(var.cluster_node_network, var.cluster_node_network_load_balancer_last_hostnum)
+          },
+        ]
+      }
+    },
+  ]
+  cilium_external_lb_manifest = join("---\n", [for d in local.cilium_external_lb_manifests : yamlencode(d)])
+}
+
+// see https://docs.siderolabs.com/kubernetes-guides/cni/deploying-cilium#method-4%3A-helm-manifests-inline-install
+// see https://docs.cilium.io/en/stable/network/servicemesh/ingress/
+// see https://docs.cilium.io/en/stable/gettingstarted/hubble_setup/
+// see https://docs.cilium.io/en/stable/gettingstarted/hubble/
+// see https://docs.cilium.io/en/stable/helm-reference/#helm-reference
+// see https://github.com/cilium/cilium/releases
+// see https://github.com/cilium/cilium/tree/v1.18.6/install/kubernetes/cilium
+// see https://registry.terraform.io/providers/hashicorp/helm/latest/docs/data-sources/template
+data "helm_template" "cilium" {
+  namespace  = "kube-system"
+  name       = "cilium"
+  repository = "https://helm.cilium.io"
+  chart      = "cilium"
+  # renovate: datasource=helm depName=cilium registryUrl=https://helm.cilium.io
+  version      = "1.18.6"
+  kube_version = var.kubernetes_version
+  api_versions = []
+  set = [
+    {
+      name  = "ipam.mode"
+      value = "kubernetes"
+    },
+    {
+      name  = "securityContext.capabilities.ciliumAgent"
+      value = "{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}"
+    },
+    {
+      name  = "securityContext.capabilities.cleanCiliumState"
+      value = "{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}"
+    },
+    {
+      name  = "cgroup.autoMount.enabled"
+      value = "false"
+    },
+    {
+      name  = "cgroup.hostRoot"
+      value = "/sys/fs/cgroup"
+    },
+    {
+      name  = "k8sServiceHost"
+      value = "localhost"
+    },
+    {
+      name  = "k8sServicePort"
+      value = local.kube_prism_port
+    },
+    {
+      name  = "kubeProxyReplacement"
+      value = "true"
+    },
+    {
+      name  = "l2announcements.enabled"
+      value = "true"
+    },
+    {
+      name  = "devices"
+      value = "{eth0}"
+    },
+    {
+      name  = "ingressController.enabled"
+      value = "true"
+    },
+    {
+      name  = "ingressController.default"
+      value = "true"
+    },
+    {
+      name  = "ingressController.loadbalancerMode"
+      value = "shared"
+    },
+    {
+      name  = "ingressController.enforceHttps"
+      value = "false"
+    },
+    {
+      name  = "envoy.enabled"
+      value = "true"
+    },
+    {
+      name  = "hubble.relay.enabled"
+      value = "true"
+    },
+    {
+      name  = "hubble.ui.enabled"
+      value = "true"
+    }
+  ]
+}
