ac5a76d1dfa44fcfc677a5909b3a4129f779a71b
vpasias <52623837+vpasias@users.noreply.github.com>
2026-02-07 13:03:05 +0200

    Update atm_vm_deployment.sh

--- atm_vm_deployment.sh
+++ atm_vm_deployment.sh
@@ -4,25 +4,37 @@ set -eux
 
 cd "$(dirname "$0")"
 
-function ssh_to() {
+function ssh_ctl() {
     local ip="10.0.123.1${1}"
     shift
     ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -l ubuntu "${ip}" "$@"
 }
 
-for i in {1..9}; do
+function ssh_ceph() {
+    local ip="10.0.123.2${1}"
+    shift
+    ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -l ubuntu "${ip}" "$@"
+}
+
+function ssh_kvm() {
+    local ip="10.0.123.3${1}"
+    shift
+    ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -l ubuntu "${ip}" "$@"
+}
+
+for i in {1..3}; do
     cat <<EOF | uvt-kvm create \
         --machine-type q35 \
-        --cpu 6 \
+        --cpu 8 \
         --host-passthrough \
-        --memory 24576 \
+        --memory 32768 \
         --disk 100 \
         --ephemeral-disk 100 \
         --ephemeral-disk 100 \
         --unsafe-caching \
         --network-config /dev/stdin \
         --no-start \
-        "node-${i}.localdomain" \
+        "ctl${i}" \
         release=jammy
 network:
   version: 2
@@ -42,48 +54,199 @@ network:
 EOF
 done
 
-for i in {1..9}; do
-    virsh detach-interface "node-${i}.localdomain" network --config
+for i in {1..3}; do
+    cat <<EOF | uvt-kvm create \
+        --machine-type q35 \
+        --cpu 4 \
+        --host-passthrough \
+        --memory 16384 \
+        --disk 100 \
+        --ephemeral-disk 100 \
+        --ephemeral-disk 100 \
+        --unsafe-caching \
+        --network-config /dev/stdin \
+        --no-start \
+        "ceph${i}" \
+        release=jammy
+network:
+  version: 2
+  ethernets:
+    enp1s0:
+      dhcp4: false
+      dhcp6: false
+      accept-ra: false
+      addresses:
+        - 10.0.123.2${i}/24
+      routes:
+        - to: default
+          via: 10.0.123.1
+      nameservers:
+        addresses:
+          - 10.0.123.1
+EOF
+done
 
-    virsh attach-interface "node-${i}.localdomain" network virbr-mgt \
-        --model virtio --config
+for i in {1..3}; do
+    cat <<EOF | uvt-kvm create \
+        --machine-type q35 \
+        --cpu 8 \
+        --host-passthrough \
+        --memory 32768 \
+        --disk 100 \
+        --ephemeral-disk 100 \
+        --ephemeral-disk 100 \
+        --unsafe-caching \
+        --network-config /dev/stdin \
+        --no-start \
+        "kvm${i}" \
+        release=jammy
+network:
+  version: 2
+  ethernets:
+    enp1s0:
+      dhcp4: false
+      dhcp6: false
+      accept-ra: false
+      addresses:
+        - 10.0.123.3${i}/24
+      routes:
+        - to: default
+          via: 10.0.123.1
+      nameservers:
+        addresses:
+          - 10.0.123.1
+EOF
+done
 
-    virsh start "node-${i}.localdomain"
+for i in {1..3}; do
+    virsh detach-interface "ctl${i}" network --config
+    
+    virsh attach-interface "ctl${i}" network virbr-mgt --model virtio --config
+
+    virsh start "ctl${i}"
+done
+
+for i in {1..3}; do
+    virsh detach-interface "ceph${i}" network --config
+    
+    virsh attach-interface "ceph${i}" network virbr-mgt --model virtio --config   
+    
+    virsh start "ceph${i}"
+done
+
+for i in {1..3}; do
+    virsh detach-interface "kvm${i}" network --config
+    
+    virsh attach-interface "kvm${i}" network virbr-mgt --model virtio --config    
+
+    virsh start "kvm${i}"
 done
 
 
-for i in {1..9}; do
-    until ssh_to "${i}" -t -- cloud-init status --wait; do
+for i in {1..3}; do
+    until ssh_ctl "${i}" -t -- cloud-init status --wait; do
         sleep 1
     done
 
-    ssh_to "${i}" -t -- sudo apt update -y
-    ssh_to "${i}" -t -- sudo apt-get install -y git vim net-tools wget curl bash-completion apt-utils iperf iperf3 mtr traceroute netcat sshpass socat
+    ssh_ctl "${i}" -t -- sudo apt update -y
+    ssh_ctl "${i}" -t -- sudo apt-get install -y git vim net-tools wget curl bash-completion apt-utils sshpass
 
-    ssh_to "${i}" -t -- 'echo "root:gprm8350" | sudo chpasswd'
-    ssh_to "${i}" -t -- 'echo "ubuntu:kyax7344" | sudo chpasswd'
-    ssh_to "${i}" -t -- "sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config"
-    ssh_to "${i}" -t -- "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
-    ssh_to "${i}" -t -- "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf"
-    ssh_to "${i}" -t -- sudo systemctl restart sshd
-    ssh_to "${i}" -t -- sudo rm -rf /root/.ssh/authorized_keys
+    ssh_ctl "${i}" -t -- 'echo "root:gprm8350" | sudo chpasswd'
+    ssh_ctl "${i}" -t -- 'echo "ubuntu:kyax7344" | sudo chpasswd'
+    ssh_ctl "${i}" -t -- "sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+    ssh_ctl "${i}" -t -- "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+    ssh_ctl "${i}" -t -- "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf"
+    ssh_ctl "${i}" -t -- sudo systemctl restart sshd
+    ssh_ctl "${i}" -t -- sudo rm -rf /root/.ssh/authorized_keys
+
+ssh_ctl "${i}" -t -- 'sudo tee -a /etc/hosts <<EOF
+10.0.123.11 ctl1 ctl1.cloud.atmosphere.dev
+10.0.123.12 ctl2 ctl2.cloud.atmosphere.dev
+10.0.123.13 ctl3 ctl3.cloud.atmosphere.dev
+10.0.123.21 ceph1 ceph1.cloud.atmosphere.dev
+10.0.123.22 ceph2 ceph2.cloud.atmosphere.dev
+10.0.123.23 ceph3 ceph3.cloud.atmosphere.dev
+10.0.123.31 kvm1 kvm1.cloud.atmosphere.dev
+10.0.123.32 kvm2 kvm2.cloud.atmosphere.dev
+10.0.123.33 kvm3 kvm3.cloud.atmosphere.dev
+EOF'
 
 done
 
-ssh_to 9 -- 'sudo tee -a /etc/hosts <<EOF
-10.0.123.11 node-1 node-1.localdomain
-10.0.123.12 node-2 node-2.localdomain
-10.0.123.13 node-3 node-3.localdomain
-10.0.123.14 node-4 node-4.localdomain
-10.0.123.15 node-5 node-5.localdomain
-10.0.123.16 node-6 node-6.localdomain
-10.0.123.17 node-7 node-7.localdomain
-10.0.123.18 node-8 node-8.localdomain
-10.0.123.19 node-9 node-9.localdomain
+for i in {1..3}; do
+    until ssh_ceph "${i}" -t -- cloud-init status --wait; do
+        sleep 1
+    done
+
+    ssh_ceph "${i}" -t -- sudo apt update -y
+    ssh_ceph "${i}" -t -- sudo apt-get install -y git vim net-tools wget curl bash-completion apt-utils sshpass
+
+    ssh_ceph "${i}" -t -- 'echo "root:gprm8350" | sudo chpasswd'
+    ssh_ceph "${i}" -t -- 'echo "ubuntu:kyax7344" | sudo chpasswd'
+    ssh_ceph "${i}" -t -- "sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+    ssh_ceph "${i}" -t -- "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+    ssh_ceph "${i}" -t -- "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf"
+    ssh_ceph "${i}" -t -- sudo systemctl restart sshd
+    ssh_ceph "${i}" -t -- sudo rm -rf /root/.ssh/authorized_keys
+
+ssh_ceph "${i}" -t -- 'sudo tee -a /etc/hosts <<EOF
+10.0.123.11 ctl1 ctl1.cloud.atmosphere.dev
+10.0.123.12 ctl2 ctl2.cloud.atmosphere.dev
+10.0.123.13 ctl3 ctl3.cloud.atmosphere.dev
+10.0.123.21 ceph1 ceph1.cloud.atmosphere.dev
+10.0.123.22 ceph2 ceph2.cloud.atmosphere.dev
+10.0.123.23 ceph3 ceph3.cloud.atmosphere.dev
+10.0.123.31 kvm1 kvm1.cloud.atmosphere.dev
+10.0.123.32 kvm2 kvm2.cloud.atmosphere.dev
+10.0.123.33 kvm3 kvm3.cloud.atmosphere.dev
 EOF'
 
-for i in {1..9}; do
+done
 
-    ssh_to "${i}" -t -- sudo reboot
+for i in {1..3}; do
+    until ssh_kvm "${i}" -t -- cloud-init status --wait; do
+        sleep 1
+    done
+
+    ssh_kvm "${i}" -t -- sudo apt update -y
+    ssh_kvm "${i}" -t -- sudo apt-get install -y git vim net-tools wget curl bash-completion apt-utils sshpass
+
+    ssh_kvm "${i}" -t -- 'echo "root:gprm8350" | sudo chpasswd'
+    ssh_kvm "${i}" -t -- 'echo "ubuntu:kyax7344" | sudo chpasswd'
+    ssh_kvm "${i}" -t -- "sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+    ssh_kvm "${i}" -t -- "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+    ssh_kvm "${i}" -t -- "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf"
+    ssh_kvm "${i}" -t -- sudo systemctl restart sshd
+    ssh_kvm "${i}" -t -- sudo rm -rf /root/.ssh/authorized_keys
+
+ssh_kvm "${i}" -t -- 'sudo tee -a /etc/hosts <<EOF
+10.0.123.11 ctl1 ctl1.cloud.atmosphere.dev
+10.0.123.12 ctl2 ctl2.cloud.atmosphere.dev
+10.0.123.13 ctl3 ctl3.cloud.atmosphere.dev
+10.0.123.21 ceph1 ceph1.cloud.atmosphere.dev
+10.0.123.22 ceph2 ceph2.cloud.atmosphere.dev
+10.0.123.23 ceph3 ceph3.cloud.atmosphere.dev
+10.0.123.31 kvm1 kvm1.cloud.atmosphere.dev
+10.0.123.32 kvm2 kvm2.cloud.atmosphere.dev
+10.0.123.33 kvm3 kvm3.cloud.atmosphere.dev
+EOF'
+
+done
+
+for i in {1..3}; do
+
+    ssh_ctl "${i}" -t -- sudo reboot
+
+done
+
+for i in {1..3}; do
+
+    ssh_ceph "${i}" -t -- sudo reboot
+
+done
+
+for i in {1..3}; do
+
+    ssh_kvm "${i}" -t -- sudo reboot
 
 done
