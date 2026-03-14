afa9bab791fd9e0a936ce1d4e96a6beddfea45f8
vpasias <52623837+vpasias@users.noreply.github.com>
2026-03-14 12:31:44 +0200

    Update create-vm.sh

--- create-vm.sh
+++ create-vm.sh
@@ -11,22 +11,19 @@ BRIDGE="service"
 
 echo "Creating VM: ${VM_NAME}..."
 
-# --- Create Virtual Disk ---
-qemu-img create -f qcow2 ${DISK_PATH} ${DISK_SIZE}G
-
 # --- Create VM with virt-install ---
 virt-install \
-  --name=${VM_NAME} \
-  --ram=${RAM} \
-  --vcpus=${VCPUS} \
-  --disk path=${DISK_PATH},format=qcow2,bus=virtio \
-  --os-variant=centos-stream9 \
-  --network bridge=${BRIDGE},model=virtio \
-  --graphics none \
-  --cdrom=${ISO_PATH} \
-  --boot hd,cdrom \
-  --console pty,target_type=serial \
-  --extra-args 'console=ttyS0,115200n8'
+--name=${VM_NAME} \
+--ram=${RAM} \
+--disk path=/mnt/extra/virt/${VM_NAME}.img,size=1300 \
+--vcpus=${VCPUS} \
+--cpu=host-passthrough \
+--os-variant=centos-stream9 \
+--network bridge=${BRIDGE},model=virtio \
+--graphics=none \
+--console=pty,target_type=serial \
+--location=${ISO_PATH} \
+--extra-args 'console=ttyS0,115200n8'
 
 echo "VM ${VM_NAME} creation initiated."
 echo "Use 'virsh list' to check status."
